efda00e35f526e988e29becb4173b059a5785070
Thorsten Krüger <thorstenkg@gmail.com>
2016-12-16 16:50:13 +0100

    cleans up recipe

--- recipes/studio.rb
+++ recipes/studio.rb
@@ -8,9 +8,7 @@ repo_pass = node['dse']['studio']['repo_pass']
 raise "need to set node['dse']['studio']['repo_user']" unless repo_user
 raise "need to set node['dse']['studio']['repo_pass']" unless repo_pass
 
-#
 install_dir = "#{node['dse']['studio']['install_dir']}/datastax-studio-#{node['dse']['studio']['version']}"
-studio_tarball = "#{node['dse']['studio']['download_to']}/datastax-studio-#{node['dse']['studio']['version']}.tar.gz"
 
 #
 # Prepare studio user
@@ -38,15 +36,11 @@ end
 # Install Studio from Tarball
 #
 
-Chef::Log.info("downloading tarball from #{node['dse']['studio']['download_url']}")
-Chef::Log.info("downloading tarball to #{studio_tarball}")
-
 tar_extract node['dse']['studio']['download_url'] do
   download_dir '/tmp/'
   target_dir node['dse']['studio']['install_dir']
   creates "#{node['dse']['studio']['install_dir']}/datastax-studio-#{node['dse']['studio']['version']}/bin/server.sh"
   user node['dse']['studio']['user']
-  #tar_flags [ '-P', '--strip-components 1' ]
 end
 
 #
